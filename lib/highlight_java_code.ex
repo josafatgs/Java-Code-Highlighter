3487e34a9cb887f5a8b7f79f204b7817b7b669d9
Josafat <josafatgarciasarmientos@gmail.com>
2023-04-21 11:37:30 -0600

    Hola mundo

---
+++ lib/highlight_java_code.ex
@@ -0,0 +1,18 @@
+defmodule HighlightJavaCode do
+  @moduledoc """
+  Documentation for `HighlightJavaCode`.
+  """
+
+  @doc """
+  Hello world.
+
+  ## Examples
+
+      iex> HighlightJavaCode.hello()
+      :world
+
+  """
+  def hello do
+    :world
+  end
+end
---
+++ test/highlight_java_code_test.exs
@@ -0,0 +1,8 @@
+defmodule HighlightJavaCodeTest do
+  use ExUnit.Case
+  doctest HighlightJavaCode
+
+  test "greets the world" do
+    assert HighlightJavaCode.hello() == :world
+  end
+end
