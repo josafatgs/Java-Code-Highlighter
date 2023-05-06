49df2f67220720216cc83e86620784876e0f64cc
jossjic <jossjic_03@hotmail.com>
2023-05-05 23:36:36 -0600

    app implementada, escript

--- mix.exs
+++ mix.exs
@@ -7,8 +7,13 @@ defmodule HighlightJavaCode.MixProject do
       version: "0.1.0",
       elixir: "~> 1.12",
       start_permanent: Mix.env() == :prod,
-      deps: deps()
+      deps: deps(),
+      escript: escript()
     ]
+  end
+
+  defp escript do
+    [main_module: HighlightJavaCode]
   end
 
   # Run "mix help compile.app" to learn about applications.
